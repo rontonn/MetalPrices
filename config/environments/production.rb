141f14b9bfc8b715e2150a3f1a6a204ab25d97d6
Anton <romanton8@gmail.com>
2013-11-27 12:56:08 +0400

    Change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Metals::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass #pre-defined NO USE
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
@@ -59,7 +59,7 @@ Metals::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( search.js ) #pre-defined NO USE
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
